932c4ac1152a37c8374bbf57a463f7e2874bf45d
Eli <eli.i.pleaner@gmail.com>
2020-04-04 11:48:22 +0200

    Custom webpack for absolute path imports

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.resolve = {
+      alias: {
+        components: path.resolve(__dirname, "src/components")
+      }
+    };
+
+    return config;
+  }
+};

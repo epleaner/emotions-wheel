7af343a872201f353ace2ad4a34dc0ed2bdb7338
Eli <eli.i.pleaner@gmail.com>
2020-04-04 12:38:40 +0200

    Add Zeit now tool, fix custom webpack

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ module.exports = {
     // Perform customizations to webpack config
     // Important: return the modified config
     config.resolve = {
+      ...config.resolve,
       alias: {
+        ...config.resolve.alias,
         components: path.resolve(__dirname, "src/components"),
         helpers: path.resolve(__dirname, "src/helpers")
       }

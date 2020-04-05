205b77391cad39f157fe47b1b1a2d3aa560c26a9
Eli <eli.i.pleaner@gmail.com>
2020-04-05 18:19:53 +0200

    Add emotion

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -1,19 +1,26 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 
-const linkStyle = {
-  marginRight: 15
-};
+const StyledAnchor = styled.a`
+  margin-right: 15px;
+`;
 
-export default () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
-    <Link href="/poems">
-      <a style={linkStyle}>Poems</a>
-    </Link>
-  </div>
-);
+const Button = styled.button`
+  color: hotpink;
+`;
+
+export default () => {
+  return (
+    <div>
+      <Link href="/">
+        <StyledAnchor>Home</StyledAnchor>
+      </Link>
+      <Link href="/about">
+        <StyledAnchor>About</StyledAnchor>
+      </Link>
+      <Link href="/poems">
+        <StyledAnchor>Poems</StyledAnchor>
+      </Link>
+    </div>
+  );
+};

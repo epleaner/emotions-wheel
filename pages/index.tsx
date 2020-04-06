486491b76ab97f648233c73c9e9ee70bb123bed0
Eli <eli.i.pleaner@gmail.com>
2020-04-06 22:50:54 +0200

    add keys

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 /** @jsx jsx */
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/core";
-import { Flex, Box, jsx } from "theme-ui";
+import { Flex, Box, Button, jsx } from "theme-ui";
 
-const fadeIn = keyframes`
+const fadeInTop = keyframes`
 from {
     transform: translate3d(0,-50px,0);
     opacity: 0;
   }
   to {
     transform: translate3d(0, 0px,0);
+    opacity: 1;
+  }
+`;
+
+const fadeInRight = keyframes`
+from {
+    transform: translate3d(-200px,0,0);
+    opacity: 0;
+  }
+  to {
+    transform: translate3d(0, 0,0);
     opacity: 1;
   }
 `;
@@ -22,7 +33,7 @@ const Container = styled(Flex)`
 `;
 
 const FadeInH1 = styled.h1`
-  animation: 2s ${fadeIn} ease;
+  animation: 2s ${fadeInTop} ease;
 `;
 
 export default () => {
@@ -35,12 +46,27 @@ export default () => {
         <Box sx={{ width: "100%" }}>
           {"how are you doing?".split(" ").map((text, index) => (
             <span
+              key={text}
               sx={{
-                animation: `2.5s ${fadeIn} ${(index + 5) * 0.2}s ease both`,
+                animation: `2.5s ${fadeInTop} ${(index + 5) * 0.2}s ease both`,
               }}
             >
               {text}{" "}
             </span>
+          ))}
+        </Box>
+        <Box mt={10} sx={{ width: "100%" }}>
+          {"good bad so-so meh".split(" ").map((text, index) => (
+            <Button
+              key={text}
+              sx={{
+                animation: `1.5s ${fadeInRight} ${
+                  (37 + index * -3) * 0.1
+                }s ease both`,
+              }}
+            >
+              {text}{" "}
+            </Button>
           ))}
         </Box>
       </Box>

3b854a9c0267389b4272368002f468227c2b9eaf
Eli <eli.i.pleaner@gmail.com>
2020-04-10 17:17:03 +0200

    Add isFetching for loading user state

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,6 +3,8 @@ import fetcher from "@helpers/fetcher";
 
 export default () => {
   const { data, mutate } = useSWR("/api/user", fetcher);
+  const isFetching = typeof data === "undefined";
+
   const user = data && data.user;
-  return [user, { mutate }];
+  return [user, { mutate }, isFetching];
 };

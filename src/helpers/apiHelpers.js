982a5f6e93d1d03c38f53f9bd2a431a2a6a8b88a
Eli <eli.i.pleaner@gmail.com>
2020-04-17 13:26:58 +0200

    Remove unused profile fields

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -1,13 +1,11 @@
 function extractUser(req) {
   if (!req.user) return null;
   // take only needed user fields to avoid sensitive ones (such as password)
-  const { name, email, bio, profilePicture, emailVerified } = req.user;
+  const { name, email, emotions } = req.user;
   return {
     name,
     email,
-    bio,
-    profilePicture,
-    emailVerified,
+    emotions,
   };
 }
 

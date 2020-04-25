af866fa34701c8229d70da71d62cf165a3db7652
Eli <eli.i.pleaner@gmail.com>
2020-04-25 18:31:21 +0200

    Remove comment

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -5,7 +5,7 @@ const MongoStore = connectMongo({ Store, MemoryStore });
 
 export default function (req, res, next) {
   const mongoStore = new MongoStore({
-    client: req.dbClient, // see how we use req.dbClient from the previous step
+    client: req.dbClient,
     stringify: false,
   });
   return session({

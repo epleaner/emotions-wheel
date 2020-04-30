73cabf519d719355a403a11745fdcea63d405a57
Eli <eli.i.pleaner@gmail.com>
2020-04-30 16:16:04 +0200

    Add delete account UI

---
+++ src/components/deleteAccountButtons.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  Input,
+} from '@chakra-ui/core';
+
+const DeleteAccountButtons = ({
+  isDisabled,
+  isLoading,
+  handleDelete,
+  confirmationName,
+}) => {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [nameConfirmed, setNameConfirmed] = useState(false);
+  const [name, setName] = useState('');
+
+  useEffect(() => setNameConfirmed(name === confirmationName), [name]);
+
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  return (
+    <Box>
+      {showDeleteConfirmation ? (
+        <>
+          <FormControl>
+            <FormHelperText id="name-helper-text">
+              Please enter your name ({confirmationName}) to confirm you want to
+              delete your account.
+            </FormHelperText>
+            <Input
+              type="text"
+              size="sm"
+              my={2}
+              value={name}
+              placeholder=""
+              aria-describedby="name-helper-text"
+              onChange={handleNameChange}
+            />
+          </FormControl>
+          <Button
+            size="xs"
+            mr={2}
+            isDisabled={isDisabled}
+            onClick={() => setShowDeleteConfirmation(false)}
+          >
+            Never mind
+          </Button>
+          <Button
+            size="xs"
+            variantColor="red"
+            onClick={handleDelete}
+            isDisabled={isDisabled || !nameConfirmed}
+            isLoading={isLoading}
+            loadingText="Deleting"
+          >
+            Yes, really delete
+          </Button>
+        </>
+      ) : (
+        <Button
+          size="xs"
+          isDisabled={isDisabled}
+          onClick={() => setShowDeleteConfirmation(true)}
+        >
+          Delete account
+        </Button>
+      )}
+    </Box>
+  );
+};
+
+export default DeleteAccountButtons;
--- src/pages/profile/edit.js
+++ src/pages/profile/edit.js
@@ -1,85 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Button, Text, Spinner } from '@chakra-ui/core';
+import React, { useState } from 'react';
+import { Text, Spinner, Divider } from '@chakra-ui/core';
 import useUser from '@hooks/useUser';
 
 import CenteredContainer from '@components/shared/centeredContainer';
 import Section from '@components/shared/section';
 import Heading from '@components/shared/heading';
 import ProfileForm from '@components/forms/profileForm';
+import DeleteAccountButtons from '@components/deleteAccountButtons';
 
 const EditProfile = () => {
   const [user, { mutate }, isFetching] = useUser();
 
-  const [isUpdating, setIsUpdating] = useState(false);
-  const [isValid, setIsValid] = useState(false);
-
-  const [msg, setMsg] = useState({ message: '', isError: false });
-  const [name, setName] = useState(user ? user.name : '');
-  const [email, setEmail] = useState(user ? user.email : '');
-  const [oldPassword, setOldPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
   const [justDeleted, setJustDeleted] = useState(false);
-
-  useEffect(() => {
-    if (!isFetching && user) {
-      setName(user.name);
-      setEmail(user.email);
-    }
-  }, [user]);
-
-  useEffect(() => {
-    let profileFieldsChanged = false;
-    let passwordsValid = false;
-
-    if (user) {
-      if (name !== user.name || email !== user.email)
-        profileFieldsChanged = true;
-
-      if (
-        (oldPassword && newPassword) ||
-        (!(oldPassword && newPassword) && profileFieldsChanged)
-      )
-        passwordsValid = true;
-    }
-
-    setIsValid(profileFieldsChanged || passwordsValid);
-  }, [user, name, email, oldPassword, newPassword]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (isUpdating) return;
-    setIsUpdating(true);
-    setMsg({ message: '', isError: false });
-
-    const body = { name, email, oldPassword, newPassword };
-
-    const response = await fetch('/api/user', {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    });
-
-    const responseJson = await response.json();
-
-    if (responseJson.ok) {
-      mutate({
-        user: {
-          ...user,
-          ...responseJson.user,
-        },
-      });
-
-      setMsg({ message: 'Profile updated' });
-      setOldPassword('');
-      setNewPassword('');
-    } else {
-      setMsg({ message: responseJson.message, isError: true });
-    }
-
-    setIsUpdating(false);
-  };
 
   const onSubmitSuccess = async (resJson) => {
     mutate({
@@ -93,22 +26,21 @@ const EditProfile = () => {
   const handleDelete = async (e) => {
     e.preventDefault();
 
-    if (isUpdating) return;
-    setIsUpdating(true);
+    setIsDeleting(true);
 
-    const response = await fetch('/api/user', {
+    const res = await fetch('/api/user', {
       method: 'DELETE',
     });
 
-    const responseJson = await response.json();
+    const resJson = await res.json();
 
-    if (responseJson.ok) {
+    if (resJson.ok) {
       setJustDeleted(true);
     } else {
-      setMsg({ message: responseJson.message, isError: true });
+      setMsg({ message: resJson.message, isError: true });
     }
 
-    setIsUpdating(false);
+    setIsDeleting(false);
   };
 
   return (
@@ -118,45 +50,18 @@ const EditProfile = () => {
       ) : justDeleted ? (
         <Text>Your account has been deleted. Sorry to see you go!</Text>
       ) : !user ? (
-        <Text>Please sign in</Text>
+        <Text>Please log in.</Text>
       ) : (
         <Section>
           <Heading mb={4}>Edit Profile</Heading>
           <ProfileForm onSubmitSuccess={onSubmitSuccess} user={user} />
-          <Box>
-            {showDeleteConfirmation ? (
-              <>
-                <Button
-                  variant="warning"
-                  disabled={isFetching}
-                  type="button"
-                  onClick={handleDelete}
-                  mt={2}
-                >
-                  Yes, really delete
-                </Button>
-                <Button
-                  variant="primary"
-                  disabled={isFetching}
-                  type="button"
-                  onClick={() => setShowDeleteConfirmation(false)}
-                  mt={2}
-                >
-                  Never mind
-                </Button>
-              </>
-            ) : (
-              <Button
-                variant="warning"
-                disabled={isFetching}
-                type="button"
-                onClick={() => setShowDeleteConfirmation(true)}
-                mt={2}
-              >
-                Delete account
-              </Button>
-            )}
-          </Box>
+          <Divider my={4} />
+          <DeleteAccountButtons
+            confirmationName={user.name}
+            isDisabled={isFetching}
+            isLoading={isDeleting}
+            handleDelete={handleDelete}
+          />
         </Section>
       )}
     </CenteredContainer>

2fa1865b08f4df6f7d31ff57e4b9c4a1fa3ed3b0
Eli <eli.i.pleaner@gmail.com>
2020-05-15 15:59:46 +0200

    Remove unused imports

--- src/pages/chart/index.js
+++ src/pages/chart/index.js
@@ -1,52 +1,14 @@
-import React, { useState } from 'react';
-import * as d3 from 'd3';
+import React from 'react';
 import { Grid, Box } from '@chakra-ui/core';
 
-import Pie from '@components/charts/pie';
-import Donut from '@components/charts/donut';
 import Sunburst from '@components/charts/sunburst';
 
-const Chart = () => {
-  const generateData = (value, length = 5) =>
-    d3.range(length).map((item) => ({
-      value: Math.random() * 100,
-      label: `label: #${item}`,
-    }));
-
-  const [data, setData] = useState(generateData());
-
-  const changeData = () => void setData(generateData());
-
-  return (
-    <>
-      <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-        <Box w='100%'>
-          <Sunburst width={800} height={800} />
-        </Box>
-        {false && (
-          <Box w='100%'>
-            <Box w='100%'>
-              <button onClick={changeData}>New Data</button>
-            </Box>
-            <Box w='50%'>
-              {
-                <Pie
-                  data={data}
-                  width={200}
-                  height={200}
-                  innerRadius={60}
-                  outerRadius={100}
-                />
-              }
-            </Box>
-            <Box w='50%'>
-              <Donut />
-            </Box>
-          </Box>
-        )}
-      </Grid>
-    </>
-  );
-};
+const Chart = () => (
+  <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+    <Box w='100%'>
+      <Sunburst />
+    </Box>
+  </Grid>
+);
 
 export default Chart;

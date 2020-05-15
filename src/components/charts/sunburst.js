01890b264066f50e122e04b56bdcd873e6e74e3a
Eli <eli.i.pleaner@gmail.com>
2020-05-15 15:51:24 +0200

    New and improved sunburst!

--- src/components/charts/sunburst.js
+++ src/components/charts/sunburst.js
@@ -1,217 +1,178 @@
-import React, {
-  useEffect,
-  useRef,
-  useState,
-  useMemo,
-  useCallback,
-} from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import jsonData from '@static/emotions.json';
 
 import * as d3 from 'd3';
 
 const Sunburst = (props) => {
-  const [sunburstSvg, setSvg] = useState(null);
   const ref = useRef(null);
 
-  const { width = 800, height = 800 } = props;
-
-  const formatNumber = useMemo(() => d3.format(',d'), []);
-
-  const x = useMemo(
-    () =>
-      d3
-        .scaleLinear()
-        .range([0, 2 * Math.PI])
-        .clamp(true),
-    []
-  );
-
-  const maxRadius = useMemo(() => Math.min(width, height) / 2 - 5, [
-    width,
-    height,
-  ]);
-
-  const y = useMemo(
-    () =>
-      d3
-        .scaleSqrt()
-        .domain([0.25, 1])
-        .range([maxRadius * 0.05, maxRadius]),
-    [maxRadius]
-  );
-
-  const root = useMemo(() => {
-    const root = d3.hierarchy(jsonData);
-    root.sum((d) => d.size);
-
-    return root;
-  }, []);
-
-  const partition = useMemo(() => d3.partition(), []);
-
-  const color = useMemo(
-    () =>
-      d3
-        .scaleOrdinal()
-        .domain(
-          partition(root)
-            .descendants()
-            .map((d) => d.data.name)
-        )
-        .range(d3.schemePaired),
-    [partition, root]
-  );
-
-  const sunburstArc = useMemo(
-    () =>
-      d3
-        .arc()
-        .startAngle((d) => x(d.x0))
-        .endAngle((d) => x(d.x1))
-        .innerRadius((d) => Math.max(0, y(d.y0)))
-        .outerRadius((d) => Math.max(0, y(d.y1)))
-        .cornerRadius(4),
-    [x, y]
-  );
-
-  const middleArcLine = useCallback(
-    (d) => {
-      const halfPi = Math.PI / 2;
-      const radius = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-
-      const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
-      const middleAngle = (angles[1] + angles[0]) / 2;
-      const invertDirection = middleAngle > 0 && middleAngle < Math.Pi; // on lower quadrants write text ccw
-      if (invertDirection) angles.reverse();
-
-      const path = d3.path();
-      path.arc(0, 0, radius, angles[0], angles[1], invertDirection);
-
-      return path.toString();
-    },
-    [x, y]
-  );
-
-  const focusOn = useCallback(
-    (svg, d = { x0: 0, x1: 1, y0: 0, y1: 1 }) => {
-      // reset to top-level if no data point specified
-
-      const transition = svg
-        .transition()
-        .duration(750)
-        .tween('scale', () => {
-          const xd = d3.interpolate(x.domain(), [d.x0, d.x1]);
-          const yd = d3.interpolate(y.domain(), [0.25, 1]);
-
-          return (t) => {
-            x.domain(xd(t));
-            y.domain(yd(t));
-          };
-        });
-
-      transition
-        .selectAll('path.main-arc')
-        .attrTween('d', (d) => () => sunburstArc(d));
-      transition
-        .selectAll('path.hidden-arc')
-        .attrTween('d', (d) => () => middleArcLine(d));
-
-      const moveStackToFront = (elementId) => {
-        svg
-          .selectAll('.slice')
-          .filter((d) => d === elementId)
-          .each((d, _, node) => {
-            if (node.parentsNode) {
-              node.parentNode.appendChild(node);
-              if (d.parent) moveStackToFront(d.parent);
-            }
-          });
-      };
-
-      moveStackToFront(d);
-    },
-    [sunburstArc, middleArcLine, x, y]
-  );
+  const { width = 800 } = props;
 
   useEffect(() => {
+    const data = jsonData;
+    const partition = (data) => {
+      const root = d3.hierarchy(data).sum((d) => d.size);
+      return d3.partition().size([2 * Math.PI, root.height + 1])(root);
+    };
+
+    const root = partition(data);
+    root.each((d) => (d.current = d));
+
+    const radius = width / 8;
+    const color = d3.scaleOrdinal(
+      d3.quantize(d3.interpolateRainbow, data.children.length + 1)
+    );
+
+    const arc = d3
+      .arc()
+      .startAngle((d) => d.x0)
+      .endAngle((d) => d.x1)
+      .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
+      .padRadius(radius * 1.5)
+      .innerRadius((d) => d.y0 * radius)
+      .outerRadius((d) => Math.max(d.y0 * radius, d.y1 * radius - 1))
+      .cornerRadius(25);
+
     const svg = d3
       .select(ref.current)
       .append('svg')
       .style('width', '100vw')
       .style('height', '100vh')
-      .attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
-      .on('click', () => focusOn(svg)); // Reset zoom on canvas click
+      .attr('viewBox', [0, 0, width, width])
+      .style('font', '10px sans-serif');
 
-    setSvg(svg);
-  }, [width, height, focusOn]);
-
-  useEffect(() => {
-    if (sunburstSvg === null) return;
-    const slice = sunburstSvg
-      .selectAll('g.slice')
-      .data(partition(root).descendants().slice(1));
-    slice.exit().remove();
-
-    const newSlices = slice
-      .enter()
+    const g = svg
       .append('g')
-      .attr('class', 'slice')
+      .attr('transform', `translate(${width / 2},${width / 2})`);
+
+    const path = g
+      .append('g')
+      .selectAll('path')
+      .data(root.descendants().slice(1))
+      .join('path')
+      .attr('fill', (d) => parentColor(d))
+      .attr('fill-opacity', (d) => opacity(d))
+      .attr('d', (d) => arc(d.current))
       .style('cursor', 'pointer')
-      .on('click', (d) => {
-        d3.event.stopPropagation();
-        focusOn(sunburstSvg, d);
+      .on('click', clicked);
+
+    path.append('title').text((d) => d.data.name);
+
+    const label = g
+      .append('g')
+      .attr('pointer-events', 'none')
+      .attr('text-anchor', 'middle')
+      .style('user-select', 'none')
+      .selectAll('text')
+      .data(root.descendants().slice(1))
+      .join('text')
+      .attr('dy', '0.35em')
+      .attr('fill-opacity', (d) => opacity(d.current))
+      .attr('transform', (d) => labelTransform(d.current))
+      .text((d) => d.data.name);
+
+    const parent = g
+      .append('circle')
+      .datum(root)
+      .attr('r', radius / 1.1)
+      .attr('fill', (d) => parentColor(d))
+      .attr('pointer-events', 'all')
+      .on('click', clicked);
+
+    const parentLabel = g
+      .append('text')
+      .datum(root)
+      .attr('pointer-events', 'none')
+      .attr('text-anchor', 'middle')
+      .style('user-select', 'none')
+      .join('text')
+      .attr('dy', '0.35em')
+      .text((d) => d.data.name);
+
+    function clicked(p) {
+      parent.datum(p.parent || root);
+
+      root.each(
+        (d) =>
+          (d.target = {
+            x0:
+              Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) *
+              2 *
+              Math.PI,
+            x1:
+              Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) *
+              2 *
+              Math.PI,
+            y0: Math.max(0, d.y0 - p.depth),
+            y1: Math.max(0, d.y1 - p.depth),
+          })
+      );
+
+      const t = g.transition().duration(750);
+      const tShort = g.transition().duration(100);
+
+      parent.transition(t).attr('fill', () => {
+        return parentColor(p);
       });
 
-    newSlices
-      .append('title')
-      .text((d) => `${d.data.name} – ${formatNumber(d.value)}`);
-    newSlices
-      .append('path')
-      .attr('class', 'main-arc')
-      .style('fill', (d) => color((d.children ? d : d.parent).data.name))
-      .style('stroke', '#fff')
-      .style('stroke-width', 1)
-      .attr('d', sunburstArc);
-    newSlices
-      .append('path')
-      .attr('class', 'hidden-arc')
-      .style('fill', 'none')
-      .attr('id', (_, i) => `hiddenArc${i}`)
-      .attr('d', middleArcLine);
+      parentLabel
+        .transition(tShort)
+        .attr('fill-opacity', () => 0)
+        .transition()
+        .delay(0)
+        .text(() => p.data.name)
+        .transition(tShort)
+        .attr('fill-opacity', () => 1);
 
-    const text = newSlices
-      .append('text')
-      .style('pointer-events', 'none')
-      .style('dominant-baseline', 'middle')
-      .style('text-anchor', 'middle');
+      // Transition the data on all arcs, even the ones that aren’t visible,
+      // so that if this transition is interrupted, entering arcs will start
+      // the next transition from the desired position.
+      path
+        .transition(t)
+        .tween('data', (d) => {
+          const i = d3.interpolate(d.current, d.target);
+          return (t) => (d.current = i(t));
+        })
+        .filter(function (d) {
+          return +this.getAttribute('fill-opacity') || arcVisible(d.target);
+        })
+        .attr('fill-opacity', (d) => (arcVisible(d.target) ? opacity(d) : 0))
+        .attrTween('d', (d) => () => arc(d.current));
 
-    // white contour
-    text
-      .append('textPath')
-      .attr('startOffset', '50%')
-      .attr('xlink:href', (_, i) => `#hiddenArc${i}`)
-      .text((d) => d.data.name)
-      .style('fill', 'none')
-      .style('stroke', '#fff')
-      .style('stroke-width', 2)
-      .style('stroke-linejoin', 'round');
+      label
+        .filter(function (d) {
+          return +this.getAttribute('fill-opacity') || labelVisible(d.target);
+        })
+        .transition(t)
+        .attr('fill-opacity', (d) => +labelVisible(d.target))
+        .attrTween('transform', (d) => () => labelTransform(d.current));
+    }
 
-    text
-      .append('textPath')
-      .attr('startOffset', '50%')
-      .attr('xlink:href', (_, i) => `#hiddenArc${i}`)
-      .text((d) => d.data.name);
-  }, [
-    sunburstSvg,
-    partition,
-    root,
-    focusOn,
-    formatNumber,
-    sunburstArc,
-    middleArcLine,
-    color,
-  ]);
+    function parentColor(d) {
+      if (d.depth === 0) return 'white';
+      while (d.depth > 1) d = d.parent;
+      return color(d.data.name);
+    }
+
+    function opacity(d) {
+      return 1 / d.depth + 0.2;
+    }
+
+    function arcVisible(d) {
+      return d.y1 <= 4 && d.y0 >= 1 && d.x1 > d.x0;
+    }
+
+    function labelVisible(d) {
+      return d.y1 <= 4 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+    }
+
+    function labelTransform(d) {
+      const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
+      const y = ((d.y0 + d.y1) / 2) * radius;
+      return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+    }
+  }, [width]);
 
   return <main ref={ref}></main>;
 };

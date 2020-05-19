254e1d908b1e0df5d7582ec0c5d31eeecdbcdd06
Eli <eli.i.pleaner@gmail.com>
2020-05-19 12:57:47 +0200

    Small styling

--- src/components/shared/footer.js
+++ src/components/shared/footer.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
-import { Flex, Text, Link as UILink } from '@chakra-ui/core';
+import { useColorMode, Flex, Button, Text, Divider } from '@chakra-ui/core';
 
-const Footer = () => (
-  <Flex p={2} as='footer' flexShrink={1}>
-    <Link href='/about'>
-      <UILink pr={3} mr={3} borderRight='text'>
-        <Text fontSize='xs'>about</Text>
-      </UILink>
-    </Link>
-    <Text fontSize='xs'>made with 💛 by eli</Text>
-  </Flex>
-);
+const Footer = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Flex px={4} py={2} as='footer' flexShrink={1}>
+      <Link href='/about'>
+        <Button
+          variantColor='black'
+          size='xs'
+          variant='link'
+          _hover={{
+            textDecoration: 'none',
+            color: `gray.${colorMode === 'light' ? 500 : 300}`,
+          }}
+        >
+          <Text textTransform='uppercase' fontSize='xs'>
+            about
+          </Text>
+        </Button>
+      </Link>
+      <Divider
+        borderColor={colorMode === 'light' ? 'gray.500' : 'white'}
+        orientation='vertical'
+      />
+      <Text textTransform='uppercase' fontSize='xs'>
+        made with 💛 by eli
+      </Text>
+    </Flex>
+  );
+};
 
 export default Footer;

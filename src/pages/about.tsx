3257b1c8c0385ab9ce1b9a4d5bb9cbec74c47673
Eli <eli.i.pleaner@gmail.com>
2020-05-20 15:54:51 +0200

    Improve about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,31 +1,33 @@
+import React from 'react';
 import { NextPage } from 'next';
-import { Flex, Link } from 'theme-ui';
+import { Link, Text } from '@chakra-ui/core';
+import Heading from '@components/shared/heading';
+import CenteredContainer from '@components/shared/centeredContainer';
+import Section from '@components/shared/section';
 
 const About: NextPage = () => (
-  <>
-    <Flex sx={{ justifyContent: 'center', flexWrap: 'wrap' }}>
-      <h1>A tool for checking in with yourself.</h1>
-    </Flex>
-    <Flex sx={{ justifyContent: 'center', flexWrap: 'wrap' }}>
-      <h2>
+  <CenteredContainer>
+    <Section>
+      <Heading as='h1' fontSize='6xl'>
+        Feels is tool for checking in with yourself.
+      </Heading>
+      <Text as='h2' fontSize='xl'>
         Inspired by{' '}
         <Link
           target='_blank'
           rel='nofollow noreferrer'
-          href='https://www.thejuntoinstitute.com'
-        >
+          href='https://www.thejuntoinstitute.com'>
           The Junto Institute's
         </Link>{' '}
         <Link
           target='_blank'
           rel='nofollow noreferrer'
-          href='https://www.thejuntoinstitute.com/blog/the-junto-emotion-wheel-why-and-how-we-use-it'
-        >
-          emotion wheel
+          href='https://www.thejuntoinstitute.com/blog/the-junto-emotion-wheel-why-and-how-we-use-it'>
+          emotion wheel.
         </Link>
-      </h2>
-    </Flex>
-  </>
+      </Text>
+    </Section>
+  </CenteredContainer>
 );
 
 export default About;

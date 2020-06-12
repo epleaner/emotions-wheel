5f7e63333626542fc18d2479922a77abd3f6b4a4
Eli <eliipleaner@gmail.com>
2020-06-12 16:08:00 +0200

    Use mobx for handling user state in login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,28 +1,38 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { observer } from 'mobx-react-lite';
+
+import { Spinner } from '@chakra-ui/core';
 
 import LoginForm from '@components/forms/loginForm';
 import CenteredContainer from '@components/shared/centeredContainer';
 import Section from '@components/shared/section';
 
-import useUser from '@hooks/useUser';
+import useCurrentUser from '@hooks/useCurrentUser';
 
 const LoginPage = () => {
   const router = useRouter();
-  const [user] = useUser();
+  const currentUserStore = useCurrentUser();
 
   useEffect(() => {
-    // redirect to home if user is authenticated
-    if (user) router.replace('/');
-  }, [user, router]);
+    if (currentUserStore.isLoggedIn) router.replace('/');
+  }, [currentUserStore.isLoggedIn, router]);
 
   return (
     <CenteredContainer>
-      <Section>
-        <LoginForm redirectTo='/' />
-      </Section>
+      {currentUserStore.isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {!currentUserStore.isLoggedIn && (
+            <Section>
+              <LoginForm redirectTo='/' />
+            </Section>
+          )}
+        </>
+      )}
     </CenteredContainer>
   );
 };
 
-export default LoginPage;
+export default observer(LoginPage);

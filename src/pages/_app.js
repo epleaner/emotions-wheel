6d13d65fb8aa0f4229867dc2ce99af1e547f7e00
Eli <eliipleaner@gmail.com>
2020-06-12 15:55:05 +0200

    Make _app a functional component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,32 @@
-import 'mobx-react/batchingForReactDom';
+import 'mobx-react-lite/batchingForReactDom';
 
 import React from 'react';
-import App from 'next/app';
+import PropTypes from 'prop-types';
+
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+
 import theme from '@styles/theme';
 import GlobalStyles from '@styles/globalStyles';
+
 import Layout from '@components/layout';
 import SEO from '@components/shared/seo';
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
 
-    return (
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <GlobalStyles />
-          <CSSReset />
-          <SEO title='feeels' />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ColorModeProvider>
-      </ThemeProvider>
-    );
-  }
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider>
+      <GlobalStyles />
+      <CSSReset />
+      <SEO title='feeels' />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ColorModeProvider>
+  </ThemeProvider>
+);
+
+MyApp.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object,
+};
 
 export default MyApp;

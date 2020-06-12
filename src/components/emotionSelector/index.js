cdf28dcf1813357bfa87454e49500e24415eb302
Eli <eliipleaner@gmail.com>
2020-06-12 11:04:56 +0200

    Show toast when an entry is saved

--- src/components/emotionSelector/index.js
+++ src/components/emotionSelector/index.js
@@ -1,20 +1,28 @@
 import React, { useState, useCallback } from 'react';
-import { Flex, Box } from '@chakra-ui/core';
+import { Flex, Box, useToast } from '@chakra-ui/core';
 
 import Sunburst from '@components/charts/sunburst';
 import SelectedBreadcrumb from '@components/emotionSelector/selectedBreadcrumb';
 import EmotionSelectionForm from '@components/forms/emotionSelectionForm';
 
 const EmotionSelector = () => {
+  const successToast = useToast();
+
   const [selected, setSelected] = useState();
   const [shouldResetSunburst, setShouldResetSunburst] = useState(false);
 
   const onSubmitSuccess = useCallback(() => {
     setShouldResetSunburst(true);
     setSelected(null);
-  }, [setSelected, setShouldResetSunburst]);
+    successToast({
+      description: 'Entry saved',
+      duration: '1500',
+      status: 'success',
+      isClosable: true,
+    });
+  }, [successToast]);
 
-  const onResetSunburst = useCallback(() => setShouldResetSunburst(false), [setShouldResetSunburst])
+  const onResetSunburst = useCallback(() => setShouldResetSunburst(false), []);
 
   return (
     <>

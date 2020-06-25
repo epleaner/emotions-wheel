3a7adfa8e1e39e6e5072002f921af6e7d6314d74
Eli <eliipleaner@gmail.com>
2020-06-25 14:06:29 +0200

    force now properly groups and avoids collisions

--- src/components/charts/beeswarm.js
+++ src/components/charts/beeswarm.js
@@ -5,10 +5,9 @@ import { observer } from 'mobx-react-lite';
 import moment from 'moment';
 import uniq from 'lodash.uniq';
 import { scaleTime } from 'd3-scale';
-import { forceSimulation, forceCollide } from 'd3-force';
+import { forceSimulation, forceCollide, forceX, forceY } from 'd3-force';
 import { axisBottom } from 'd3-axis';
 import { timeFormat } from 'd3-time-format';
-import { timeDay } from 'd3-time';
 import { extent } from 'd3-array';
 import { select as d3Select } from 'd3-selection';
 import { easeCubicOut } from 'd3-ease';
@@ -23,7 +22,7 @@ const Beeswarm = ({ data }) => {
   const width = 600;
   const height = 200;
   const margin = 50;
-  const radius = 5;
+  const radius = 10;
   const svgRef = useRef(null);
 
   data = data.map((d) => ({
@@ -50,11 +49,6 @@ const Beeswarm = ({ data }) => {
         .tickValues(uniq(data.map((d) => d.date)))
         .tickFormat(timeFormat('%m/%d')),
     [xScale, data]
-  );
-
-  const forceSim = useMemo(
-    () => forceSimulation(data).force('collide', forceCollide(5)),
-    [data]
   );
 
   const showTooltip = useMemo(
@@ -102,9 +96,16 @@ const Beeswarm = ({ data }) => {
       .append('circle')
       .attr('r', radius)
       .attr('fill', (d) => d.color)
-      .attr('cx', (d) => xScale(d.date))
-      .attr('cy', height / 2)
       .attr('class', 'circles');
+
+    forceSimulation(data)
+      .force('collide', forceCollide(radius))
+      .force('x', forceX((d) => xScale(d.date)).strength(1))
+      .force('y', forceY(height / 2).strength(1))
+      .on('tick', () => {
+        circles.attr('cx', (d) => d.x);
+        circles.attr('cy', (d) => d.y);
+      });
 
     circles.attr('opacity', 0).transition().duration(1000).attr('opacity', 1);
 
@@ -179,17 +180,12 @@ const Beeswarm = ({ data }) => {
       .transition()
       .ease(easeCubicOut)
       .duration(750)
-      .attr('cx', (d) => d.x)
-      .attr('cy', (d) => height / 2 + d.y);
+      .attr('cx', (d) => d.x);
 
     enteredNodes
       .merge(updatedNodes)
       .on('mouseover', showTooltip)
       .on('mouseout', hideTooltip);
-
-    forceSim.on('tick', () => {
-      circles.attr('cy', (d) => d.y);
-    });
 
     g.append('g')
       .attr('transform', `translate(0, ${height - margin})`)
@@ -198,7 +194,7 @@ const Beeswarm = ({ data }) => {
     return () => {
       d3Select(svg).remove();
     };
-  }, [data, width, xAxis, forceSim, xScale, showTooltip, hideTooltip]);
+  }, [data, width, xAxis, xScale, showTooltip, hideTooltip]);
 
   return <main ref={svgRef}></main>;
 };

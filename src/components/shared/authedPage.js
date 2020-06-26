24424de2b994575e5d9ee65f9c51593ae8bf73bd
Eli <eliipleaner@gmail.com>
2020-06-26 13:17:18 +0200

    Style auth page to stack content

--- src/components/shared/authedPage.js
+++ src/components/shared/authedPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 
 import Link from 'next/link';
-import { Link as UILink, Spinner, Text } from '@chakra-ui/core';
+import { Link as UILink, Spinner, Text, Stack } from '@chakra-ui/core';
 
 import useCurrentUser from '@hooks/useCurrentUser';
 import CenteredContainer from '@components/shared/centeredContainer';
@@ -20,18 +20,20 @@ const AuthedPage = ({ render }) => {
   if (!userStore.isLoggedIn)
     return (
       <CenteredContainer>
-        <Text fontSize='6xl'>Welcome!</Text>
-        <Text fontSize='5xl'>
-          Please{' '}
-          <Link href='/login'>
-            <UILink color='blue.200'>log in</UILink>
-          </Link>{' '}
-          or{' '}
-          <Link href='/signup'>
-            <UILink color='green.200'>sign up</UILink>
-          </Link>{' '}
-          first.
-        </Text>
+        <Stack>
+          <Text fontSize='6xl'>Welcome!</Text>
+          <Text fontSize='5xl'>
+            Please{' '}
+            <Link href='/login'>
+              <UILink color='blue.200'>log in</UILink>
+            </Link>{' '}
+            or{' '}
+            <Link href='/signup'>
+              <UILink color='green.200'>sign up</UILink>
+            </Link>{' '}
+            first.
+          </Text>
+        </Stack>
       </CenteredContainer>
     );
 

0e3a28e7e3cf8e38e078f90f8777f022f57bdfbe
Eli <eliipleaner@gmail.com>
2020-07-15 15:31:21 +0200

    Synchronize form UI colors with most recently selected emotion

--- src/components/forms/emotionSelectionForm.js
+++ src/components/forms/emotionSelectionForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useMemo, useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react-lite';
 import { Formik, Form, Field } from 'formik';
@@ -17,137 +17,142 @@ import {
 import LoginSignupModal from '@components/modals/loginSignupModal';
 import Fade from '@components/shared/transitions/fade';
 
-const EmotionSelectionForm = observer(({ selected, onSubmitSuccess }) => {
-  const userStore = useCurrentUser();
+const EmotionSelectionForm = observer(
+  ({ selected, allSelected, onSubmitSuccess }) => {
+    const userStore = useCurrentUser();
 
-  const { colorMode } = useColorMode();
+    const { colorMode } = useColorMode();
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [formErrorMessage, setFormErrorMessage] = useState('');
-  const [formValues, setFormValues] = useState();
-  const [isMounted, setIsMounted] = useState(false);
+    const [formErrorMessage, setFormErrorMessage] = useState('');
+    const [formValues, setFormValues] = useState();
+    const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => setIsMounted(true), []);
+    useEffect(() => setIsMounted(true), []);
 
-  const handleSubmit = useCallback(async () => {
-    try {
-      const res = await fetch('/api/emotions', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          ...formValues,
-          emotion: selected,
-        }),
-      });
+    const handleSubmit = useCallback(async () => {
+      try {
+        const res = await fetch('/api/emotions', {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            ...formValues,
+            emotion: selected,
+          }),
+        });
 
-      switch (res.status) {
-        case 201: {
-          if (userStore.isLoggedIn) {
-            const resJson = await res.json();
-            userStore.addEmotion(resJson);
+        switch (res.status) {
+          case 201: {
+            if (userStore.isLoggedIn) {
+              const resJson = await res.json();
+              userStore.addEmotion(resJson);
+            }
+            break;
           }
-          break;
+          default:
+            setFormErrorMessage('Something went wrong, please try again');
+            break;
         }
-        default:
-          setFormErrorMessage('Something went wrong, please try again');
-          break;
+      } catch (e) {
+        setFormErrorMessage('Something went wrong, please try again');
       }
-    } catch (e) {
-      setFormErrorMessage('Something went wrong, please try again');
-    }
-  }, [formValues, userStore, selected]);
+    }, [formValues, userStore, selected]);
 
-  return (
-    <>
-      <LoginSignupModal
-        {...{
-          isOpen,
-          onClose,
-          onSubmitSuccess: handleSubmit,
-          emotionFormValues: { ...formValues, emotion: selected },
-        }}
-      />
-      <Formik
-        initialValues={{ note: '' }}
-        validate={(values) => {
-          setFormValues(values);
-          setFormErrorMessage(null);
-        }}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {
-          setFormValues(values);
+    const selectedColor = useMemo(() => {
+      if (selected) return selected.color;
+      else if (allSelected.length > 0)
+        return allSelected[allSelected.length - 1].color;
 
-          if (!userStore.isLoggedIn) onOpen();
-          else {
-            await handleSubmit();
-            await onSubmitSuccess();
-            resetForm();
-            setSubmitting(false);
-          }
-        }}>
-        {({ isSubmitting, errors }) => (
-          <Form>
-            <Field name='note'>
-              {({ field }) => (
-                <FormControl>
-                  <Fade in={isMounted}>
-                    <Flex w='100%' alignItems='baseline'>
-                      <Input
-                        aria-label='Note'
-                        id='name'
-                        type='text'
-                        variant='flushed'
-                        width={['100%', '100%', 600]}
-                        borderColor={
-                          selected
-                            ? selected.color
-                            : colorMode === 'light'
-                            ? 'gray.100'
-                            : 'gray.500'
-                        }
-                        focusBorderColor={
-                          selected ? selected.color : 'gray.100'
-                        }
-                        mr={2}
-                        size='xs'
-                        {...field}
-                        placeholder='Want to talk about it?'
-                        data-testid='emotion-selection-input'
-                      />
-                      <Button
-                        variant='outline'
-                        borderColor={selected ? selected.color : 'gray.400'}
-                        _hover={{ bg: selected ? selected.color : 'gray.100' }}
-                        size='sm'
-                        isDisabled={
-                          !selected ||
-                          Object.entries(errors).length ||
-                          isSubmitting ||
-                          userStore.isLoading
-                        }
-                        isLoading={isSubmitting}
-                        loadingText='Saving'
-                        type='submit'
-                        data-testid='emotion-selection-submit'>
-                        Save
-                      </Button>
-                    </Flex>
-                  </Fade>
-                </FormControl>
-              )}
-            </Field>
-            <FormControl isInvalid={formErrorMessage}>
-              <FormErrorMessage>{formErrorMessage}</FormErrorMessage>
-            </FormControl>
-          </Form>
-        )}
-      </Formik>
-    </>
-  );
-});
+      return colorMode === 'light' ? 'gray.100' : 'gray.500';
+    }, [selected, allSelected, colorMode]);
+
+    return (
+      <>
+        <LoginSignupModal
+          {...{
+            isOpen,
+            onClose,
+            onSubmitSuccess: handleSubmit,
+            emotionFormValues: { ...formValues, emotion: selected },
+          }}
+        />
+        <Formik
+          initialValues={{ note: '' }}
+          validate={(values) => {
+            setFormValues(values);
+            setFormErrorMessage(null);
+          }}
+          onSubmit={async (values, { setSubmitting, resetForm }) => {
+            setFormValues(values);
+
+            if (!userStore.isLoggedIn) onOpen();
+            else {
+              await handleSubmit();
+              await onSubmitSuccess();
+              resetForm();
+              setSubmitting(false);
+            }
+          }}>
+          {({ isSubmitting, errors }) => (
+            <Form>
+              <Field name='note'>
+                {({ field }) => (
+                  <FormControl>
+                    <Fade in={isMounted}>
+                      <Flex w='100%' alignItems='baseline'>
+                        <Input
+                          aria-label='Note'
+                          id='name'
+                          type='text'
+                          variant='flushed'
+                          width={['100%', '100%', 600]}
+                          borderColor={selectedColor}
+                          focusBorderColor={selectedColor}
+                          mr={2}
+                          size='xs'
+                          {...field}
+                          placeholder='Want to talk about it?'
+                          data-testid='emotion-selection-input'
+                        />
+                        <Button
+                          variant='outline'
+                          borderColor={selectedColor}
+                          _hover={{
+                            bg: selectedColor,
+                          }}
+                          size='sm'
+                          isDisabled={
+                            (allSelected.length === 0 && !selected) ||
+                            Object.entries(errors).length ||
+                            isSubmitting ||
+                            userStore.isLoading
+                          }
+                          isLoading={isSubmitting}
+                          loadingText='Saving'
+                          type='submit'
+                          data-testid='emotion-selection-submit'>
+                          Save
+                        </Button>
+                      </Flex>
+                    </Fade>
+                  </FormControl>
+                )}
+              </Field>
+              <FormControl isInvalid={formErrorMessage}>
+                <FormErrorMessage>{formErrorMessage}</FormErrorMessage>
+              </FormControl>
+            </Form>
+          )}
+        </Formik>
+      </>
+    );
+  }
+);
 
 EmotionSelectionForm.propTypes = {
   selected: PropTypes.object,
+  allSelected: PropTypes.array,
   onSubmitSuccess: PropTypes.func.isRequired,
 };
 

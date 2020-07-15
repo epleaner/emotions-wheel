7bc6e33aeba12fd5d69ccc869583b7337211ff12
Eli <eliipleaner@gmail.com>
2020-07-15 15:30:47 +0200

    Able to select and remove multiple emotions within sunburst

--- src/components/emotionSelector/index.js
+++ src/components/emotionSelector/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useCallback } from 'react';
-import { Flex, Box, useToast } from '@chakra-ui/core';
+import React, { useMemo, useState, useCallback } from 'react';
+import { Flex, Box, useToast, IconButton, Stack } from '@chakra-ui/core';
 
 import Sunburst from '@components/charts/sunburst';
 import SelectedBreadcrumb from '@components/emotionSelector/selectedBreadcrumb';
@@ -9,6 +9,8 @@ const EmotionSelector = () => {
   const successToast = useToast();
 
   const [selected, setSelected] = useState();
+  const [allSelected, setAllSelected] = useState([]);
+
   const [shouldResetSunburst, setShouldResetSunburst] = useState(false);
 
   const onSubmitSuccess = useCallback(() => {
@@ -24,6 +26,38 @@ const EmotionSelector = () => {
 
   const onResetSunburst = useCallback(() => setShouldResetSunburst(false), []);
 
+  const alreadySelected = useMemo(
+    () =>
+      selected &&
+      allSelected.some(
+        (s) =>
+          s.data[s.data.length - 1] === selected.data[selected.data.length - 1]
+      ),
+    [selected, allSelected]
+  );
+
+  const onClickAddSelected = useCallback(() => {
+    if (alreadySelected) console.log('already selected');
+    else {
+      setAllSelected([...allSelected, selected]);
+      setSelected(null);
+      setShouldResetSunburst(true);
+    }
+  }, [allSelected, selected, alreadySelected]);
+
+  const removeSelected = useCallback(
+    (selected) => {
+      setAllSelected(
+        allSelected.filter(
+          (s) =>
+            s.data[s.data.length - 1] !==
+            selected.data[selected.data.length - 1]
+        )
+      );
+    },
+    [allSelected]
+  );
+
   return (
     <>
       <Box w='100%'>
@@ -34,11 +68,43 @@ const EmotionSelector = () => {
           onSelect={setSelected}
         />
       </Box>
-      <Flex w='100%' h={30} justifyContent='center' alignItems='baseline'>
-        {selected && <SelectedBreadcrumb {...{ selected }} />}
+      <Flex w='100%' minH={30} justifyContent='center' alignItems='baseline'>
+        <Stack justify='center' align='center'>
+          {allSelected.map((selected) => (
+            <Stack key={selected.data} isInline justify='center'>
+              <IconButton
+                aria-label='Remove this emotion'
+                icon='small-close'
+                isRound
+                size='xs'
+                variant='ghost'
+                variantColor='grayscale'
+                onClick={() => {
+                  removeSelected(selected);
+                }}
+              />
+              <SelectedBreadcrumb {...{ selected }} />
+            </Stack>
+          ))}
+          {selected && (
+            <>
+              <SelectedBreadcrumb {...{ selected }} />
+              <IconButton
+                aria-label='Select another emotion'
+                icon='small-add'
+                mt={3}
+                isRound
+                size='sm'
+                variant='outline'
+                disabled={alreadySelected}
+                onClick={() => onClickAddSelected()}
+              />
+            </>
+          )}
+        </Stack>
       </Flex>
       <Flex w='100%' mt={4} justifyContent='center'>
-        <EmotionSelectionForm {...{ selected, onSubmitSuccess }} />
+        <EmotionSelectionForm {...{ selected, allSelected, onSubmitSuccess }} />
       </Flex>
     </>
   );

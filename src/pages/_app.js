a2524c252888f34a45154ea8dbecf11b817621b7
Eli <eliipleaner@gmail.com>
2020-07-30 12:49:24 +0200

    Replace ThemeProvider with ChakraProvider, use chakra theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,25 +3,24 @@ import 'mobx-react-lite/batchingForReactDom';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+import { ChakraProvider, CSSReset } from '@chakra-ui/core';
 
-import theme from '@styles/theme';
+// import theme from '@styles/theme';
+import theme from '../../chakra';
 import GlobalStyles from '@styles/globalStyles';
 
 import Layout from '@components/layout';
 import SEO from '@components/shared/seo';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <ColorModeProvider>
-      <GlobalStyles />
-      <CSSReset />
-      <SEO title='feeels' />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ColorModeProvider>
-  </ThemeProvider>
+  <ChakraProvider theme={theme}>
+    <GlobalStyles />
+    <CSSReset />
+    <SEO title='feeels' />
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </ChakraProvider>
 );
 
 MyApp.propTypes = {

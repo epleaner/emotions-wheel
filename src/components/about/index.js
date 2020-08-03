d0d5b12813c5332b65658f720438c6936c3ca64e
Eli <eliipleaner@gmail.com>
2020-08-03 13:10:05 +0200

    Make heights of blurb sections responsive

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,25 +7,27 @@ import Mountains from '@components/about/scenery/mountains';
 import MainHeading from '@components/about/headings/main';
 import SecondHeading from '@components/about/headings/second';
 
-const About = () => (
-  <Flex>
-    <Section>
-      <Flex minHeight='85vh' alignItems='center'>
-        <Box mx={[3, 5, 20, 32]}>
-          <Clouds />
-          <MainHeading />
-        </Box>
-      </Flex>
-      <Flex minHeight='85vh' alignItems='center'>
-        <Box>
-          <Mountains />
-          <Box mx={[3, 5, 20, 32]}>
-            <SecondHeading />
-          </Box>
-        </Box>
-      </Flex>
-    </Section>
+const BlurbSection = ({ children }) => (
+  <Flex minHeight={['70vh', '85vh']} alignItems='center'>
+    <Box>{children}</Box>
   </Flex>
+);
+
+const About = () => (
+  <Section>
+    <BlurbSection>
+      <Box mx={[3, 5, 20, 32]}>
+        <Clouds />
+        <MainHeading />
+      </Box>
+    </BlurbSection>
+    <BlurbSection>
+      <Mountains />
+      <Box mx={[3, 5, 20, 32]}>
+        <SecondHeading />
+      </Box>
+    </BlurbSection>
+  </Section>
 );
 
 export default About;

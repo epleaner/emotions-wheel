1f03f1f2028af46af5ed6b654493ff9109694bb6
Eli <eliipleaner@gmail.com>
2020-08-04 13:11:21 +0200

    Clouds now have dark mode styles

--- src/components/about/scenery/clouds.js
+++ src/components/about/scenery/clouds.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+
+import { useColorMode } from '@chakra-ui/core';
 
 import Cloud1 from '@static/icons/cloud_1.svg';
 import Cloud2 from '@static/icons/cloud_2.svg';
 import CloudSun from '@static/icons/cloud_sun.svg';
+import CloudMoon from '@static/icons/cloud_moon.svg';
 
 import { Flex, Box } from '@chakra-ui/core';
 import Section from '@components/shared/section';
 
-const Clouds = () => (
-  <Section>
-    <Flex justify='space-between' flexWrap='wrap'>
-      <Box width={['100px', '100px', '125px', '150px']}>
-        <Cloud1 />
-      </Box>
+const Clouds = () => {
+  const { colorMode } = useColorMode();
+
+  const CloudContainer = useMemo(
+    () => ({ children, ...otherProps }) => (
       <Box
-        mt='-50px'
-        ml={['60px', '60px', '60px', '200px']}
-        width={['100px', '100px', '125px', '150px']}>
-        <Cloud2 />
+        width={['100px', '100px', '125px', '150px']}
+        style={{
+          opacity: '0.25',
+          fill: colorMode === 'dark' ? '#f1eded' : 'black',
+        }}
+        {...otherProps}>
+        {children}
       </Box>
-      <Box mt='20px' width={['100px', '100px', '125px', '150px']}>
-        <CloudSun />
-      </Box>
-    </Flex>
-  </Section>
-);
+    ),
+    [colorMode]
+  );
+
+  return (
+    <Section>
+      <Flex justify='space-between' flexWrap='wrap'>
+        <CloudContainer>
+          <Cloud1 />
+        </CloudContainer>
+        <CloudContainer mt='-50px' ml={['60px', '60px', '60px', '200px']}>
+          <Cloud2 />
+        </CloudContainer>
+        <CloudContainer mt='20px'>
+          {colorMode === 'dark' ? <CloudMoon /> : <CloudSun />}
+        </CloudContainer>
+      </Flex>
+    </Section>
+  );
+};
 
 export default Clouds;

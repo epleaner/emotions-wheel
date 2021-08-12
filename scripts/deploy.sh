294e2790b571ba432b073f15ad5842c550abc82b
mykeels <mykehell123@gmail.com>
2021-08-12 12:42:06 +0100

    chore: restart pm2 app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SITE_DIR=$1
 
+if [ -f "$DIR/.env" ]
+then
+    # read .env file if exists
+    source $DIR/.env
+fi
+
 if [ -z "$SITE_DIR" ]
 then
     echo "Please specify a website directory"
@@ -14,3 +20,14 @@ git fetch --all
 git reset --hard origin/master
 yarn
 yarn build
+
+# (re)start pm2 instance
+PM2_APP_NAME=emotions
+pm2 describe $PM2_APP_NAME > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start yarn --name $PM2_APP_NAME --interpreter bash -- serve -p $PORT
+else
+  pm2 restart $PM2_APP_NAME
+fi
